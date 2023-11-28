f2166ba190768044096ed0f64d82685cf3a960e6
mamun-prodhan <mamunpr9@gmail.com>
2023-11-28 11:18:43 +0600

    create donation request implemented

--- index.js
+++ index.js
@@ -25,13 +25,72 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const usersCollection = client.db("assignment-12").collection("users");
+    const upazilasCollection = client
+      .db("assignment-12")
+      .collection("upazilas");
+    const districtsCollection = client
+      .db("assignment-12")
+      .collection("districts");
+    const donationRequestCollection = client
+      .db("assignment-12")
+      .collection("donationRequest");
 
-    // user data
+    //   get upazila and district data----------------------------------------------
+    app.get("/upazilas", async (req, res) => {
+      const result = await upazilasCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/districts", async (req, res) => {
+      const result = await districtsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // user data---------------------------------------------------------------------
+    app.get("/users", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       try {
         const usersData = req.body;
         console.log(usersData);
         const result = await usersCollection.insertOne(usersData);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+        res.send(error);
+      }
+    });
+
+    app.patch("/users", async (req, res) => {
+      const email = req.query.email;
+      const updatedUser = req.body;
+      const filter = { email: email };
+      const updatedDoc = {
+        $set: {
+          name: updatedUser.name,
+          email: updatedUser.email,
+          bloodGroup: updatedUser.bloodGroup,
+          photo: updatedUser.photo,
+          upazila: updatedUser.upazila,
+          district: updatedUser.district,
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    // donation data posting
+    app.post("/create-donation-request", async (req, res) => {
+      try {
+        const donationRequest = req.body;
+        console.log(donationRequest);
+        const result = await donationRequestCollection.insertOne(
+          donationRequest
+        );
         res.send(result);
       } catch (error) {
         console.log(error);
