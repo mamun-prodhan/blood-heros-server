cf2989ca6ad5addf92a728387aab703ee4fd6746
mamun-prodhan <mamunpr9@gmail.com>
2023-11-30 20:28:41 +0600

    verify token and verify admin implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -35,6 +36,42 @@ async function run() {
       .db("assignment-12")
       .collection("donationRequest");
     const blogsCollection = client.db("assignment-12").collection("blogs");
+    // JWT related api ---------------------------------------------------------
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // middlewares--------------------------------------------------------------
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
 
     //   get upazila and district data----------------------------------------------
     app.get("/upazilas", async (req, res) => {
@@ -47,7 +84,7 @@ async function run() {
     });
 
     // user data---------------------------------------------------------------------
-    app.get("/all-users", async (req, res) => {
+    app.get("/all-users", verifyToken, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
